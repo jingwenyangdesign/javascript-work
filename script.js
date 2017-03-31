04e5accf220e6e5f4cfe5fd27908efb642928a1e
Jingwen Yang <jyang77@id.iit.edu>
2017-03-31 11:40:18 -0500

    create a click event listener for the first box

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ var boxes = document.querySelectorAll(".box");
 boxes[0].style.backgroundColor ="blue";
 boxes[2].style.backgroundColor ="yellow";
 
+function boxClicked(){
+    
+}
+
+
+boxes[0].addEventListener("click", boxClicked);
+
+
+
 console.log(boxes[0]);
